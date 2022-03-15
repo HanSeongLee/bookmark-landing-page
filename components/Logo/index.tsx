e1a61bb9928d540bc3733ede7b7c40783b298392
HanSeong Lee <jkhanseong@gmail.com>
2022-03-15 21:21:26 +0900

    ADD | Inherit HTML attributes on logo

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {HTMLAttributes} from "react";
 import Link from 'next/link';
 
-const Logo: React.FC = () => {
+interface IProps extends HTMLAttributes<HTMLImageElement> {
+    src?: string;
+};
+
+const Logo: React.FC<IProps> = ({ src, ...props }) => {
     return (
         <Link href={'/'}>
             <a>
-                <img src={'/logo-bookmark.svg'}
+                <img src={src}
                      alt={'bookmark'}
+                     {...props}
                 />
             </a>
         </Link>
     );
+};
+
+Logo.defaultProps = {
+    src: '/logo-bookmark.svg',
 };
 
 export default Logo;
