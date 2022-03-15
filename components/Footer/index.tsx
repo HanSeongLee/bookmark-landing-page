25c53a04551fa69c4828e3bfa67bfe85f96f6cb6
HanSeong Lee <jkhanseong@gmail.com>
2022-03-15 21:27:23 +0900

    ADD | Footer on mobile

---
+++ components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from './style.module.scss';
+import Container from "../Container";
+import SitemapContainer from "../../containers/SitemapContainer";
+import Logo from "../Logo";
+import SocialContainer from "../../containers/SocialContainer";
+
+const Footer: React.FC = () => {
+    return (
+        <footer className={styles.footer}>
+            <Container>
+                <Logo className={styles.logo}
+                      src={'/footer-logo.svg'}
+                />
+
+                <SitemapContainer className={styles.sitemapContainer} />
+
+                <SocialContainer className={styles.socialContainer} />
+            </Container>
+        </footer>
+    );
+};
+
+export default Footer;
