08518e22b41a011bd48d74b30cc3a17116fd6b77
HanSeong Lee <jkhanseong@gmail.com>
2022-03-15 20:57:33 +0900

    ADD | Contact section on mobile

---
+++ components/ContactSection/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from './style.module.scss';
+import Container from "../Container";
+import ContactContainer from "../../containers/ContactContainer";
+
+const ContactSection: React.FC = () => {
+    return (
+        <section className={styles.contactSection}>
+            <Container>
+                <div className={styles.subtitle}>
+                    35,000+ ALREADY JOINED
+                </div>
+                <h2 className={styles.title}>
+                    Stay up-to-date with what
+                    we’re doing
+                </h2>
+                <ContactContainer className={styles.contactContainer} />
+            </Container>
+        </section>
+    );
+};
+
+export default ContactSection;
