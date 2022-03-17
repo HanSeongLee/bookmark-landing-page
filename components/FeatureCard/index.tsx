b7a045e6c862750533009287664d6b0716cca872
HanSeong Lee <jkhanseong@gmail.com>
2022-03-17 19:04:38 +0900

    ADD | Feature card on desktop

--- components/FeatureCard/index.tsx
+++ components/FeatureCard/index.tsx
@@ -1,6 +1,7 @@
 import React, {HTMLAttributes} from "react";
 import styles from './style.module.scss';
 import Button from "../Button";
+import Container from "../Container";
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
     src: string;
@@ -12,25 +13,27 @@ interface IProps extends HTMLAttributes<HTMLDivElement> {
 const FeatureCard: React.FC<IProps> = ({ src, title, description, buttonText }) => {
     return (
         <div className={styles.featureCard}>
-            {src && (
-                <div className={styles.featureImageContainer}>
-                    <img className={styles.featureImage}
-                         src={src}
-                         alt={''}
-                    />
+            <Container className={styles.container}>
+                {src && (
+                    <div className={styles.featureImageContainer}>
+                        <img className={styles.featureImage}
+                             src={src}
+                             alt={''}
+                        />
+                    </div>
+                )}
+                <div className={styles.content}>
+                    <h3 className={styles.title}>
+                        {title}
+                    </h3>
+                    <p className={styles.description}>
+                        {description}
+                    </p>
+                    <Button>
+                        {buttonText}
+                    </Button>
                 </div>
-            )}
-            <div>
-                <h3 className={styles.title}>
-                    {title}
-                </h3>
-                <p className={styles.description}>
-                    {description}
-                </p>
-                <Button>
-                    {buttonText}
-                </Button>
-            </div>
+            </Container>
         </div>
     );
 };
